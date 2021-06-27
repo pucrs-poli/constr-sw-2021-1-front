ece6709afb38a99de61bdd0dc34f66674eaf9d51
josephaffeldtw <josephaffeldtw@gmail.com>
2021-06-27 18:07:16 -0300

    Iniciando botão de confirmação

--- avaliacoes/src/app/shared/components/primary-button/primary-button.component.ts
+++ avaliacoes/src/app/shared/components/primary-button/primary-button.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-primary-button',
@@ -7,9 +7,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PrimaryButtonComponent implements OnInit {
 
+  @Input() text: string = '';
+  @Input() icon: string = '';
+
+  @Output() clicked: EventEmitter<any> = new EventEmitter();
+  
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnInit() {
   }
 
 }
