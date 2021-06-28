ca6c92bdcef196df01fd501b98ec3fae1a259c78
biancacamargomachado <bianca.camargo@acad.pucrs.br>
2021-06-27 21:25:34 -0300

    create tabs component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { MatTableModule } from '@angular/material/table';
+import { MatTabsModule } from '@angular/material/tabs';
 import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ResourceTableComponent } from './components/resource-table/resource-table.component';
+import { ResourceTabsComponent } from './components/resource-tabs/resource-tabs.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ResourceTableComponent
+    ResourceTableComponent,
+    ResourceTabsComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     MatTableModule,
+    MatTabsModule,
     HttpClientModule,
   ],
   providers: [],
---
+++ src/app/components/resource-tabs/resource-tabs.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-resource-tabs',
+  templateUrl: './resource-tabs.component.html',
+  styleUrls: ['./resource-tabs.component.css']
+})
+export class ResourceTabsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
